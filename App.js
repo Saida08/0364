27e9154a5ef839b14b9381817d60aaadcdf7f18d
Saida08 <153554764+Saida08@users.noreply.github.com>
2024-02-09 06:04:59 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,21 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-import Week1 from './components/week1';
+import saida from './saida.jpg';
 
 function App() {
   return (
-    <div className="App">
-      <h1>HELLO WORLD!</h1>
-      <h2>M.Saida Reddy</h2>
-      <h2>2211CS010364</h2>
-      <h2>Group-7</h2>
-      <hr/>
-      <Week1/>
+    <div>
+      <header>
+        <h1>Hello World</h1>
+        <p>Name: M.Saida Reddy<br></br>
+        Ht.no:2211CS010364,<br></br>
+        Group:7A</p>
+        <hr></hr>
+        <img src={saida} alt="saida" />
+        <h2>About Myself</h2>
+        <p>I am Saida from group 7A,<br></br>
+          I am 18 years old,<br></br>
+          I like reading books,playing sports etc...
+        </p>
+      
+      </header>
     </div>
-   
-   
-    
   );
 }
 
-export default App;
+export default App;
